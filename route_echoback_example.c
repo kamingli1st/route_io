b92f1440514d0710b578d35e5a48121c316f5933
Woon Sze Hsiang <woonsh@comfortdelgro.com>
2018-08-17 17:15:56 +0800

    update example

--- route_echoback_example.c
+++ route_echoback_example.c
@@ -11,38 +11,37 @@ void on_conn_close_handler(rio_request_t *req);
 void init_instance(void *arg);
 
 void init_instance(void *arg) {
-  fprintf(stderr, "%s\n", "start instance");
+	printf("%s\n", "start instance");
 }
 
 void read_handler(rio_request_t *req) {
-  /** To varable context reference until the connection closed, it will trigger free context handler **/
-  if (req->ctx_val == NULL) {
-    req->ctx_val = malloc(1024 * sizeof(char));
-  }
-  // printf("%d,  %.*s\n", i++, (int) (req->in_buff->end - req->in_buff->start), req->in_buff->start);
-  rio_write_output_buffer_l(req, req->in_buff->start, (req->in_buff->end - req->in_buff->start));
-  rio_write_output_buffer(req, (unsigned char*)"\n");
+	int nbytes = rio_buf_size(req->in_buff);
+	printf("Readed bytes is %d\n", nbytes);
+
+	rio_write_output_buffer_l(req, req->in_buff->start, nbytes);
+
+	rio_write_output_buffer(req, "\n");
+
 }
 
 void on_conn_close_handler(rio_request_t *req) {
-  if (req->ctx_val) {
-    free(req->ctx_val);
-  }
-  fprintf(stderr, "%s\n", "Connection closing");
+	if (req->ctx_val) {
+		free(req->ctx_val);
+		req->ctx_val = NULL;
+	}
+// fprintf(stderr, "%s\n", "Connection closing");
 }
 
 int main(void) {
+	rio_set_max_polling_event(1024);
+	rio_set_sz_per_read(2048);
 
-  rio_instance_t * instance = rio_create_routing_instance(init_instance, NULL);
-#if defined _WIN32 || _WIN64 /*Windows*/
-  rio_add_udp_fd(instance, 12345, read_handler, 64, 1024, on_conn_close_handler);
-  rio_add_tcp_fd(instance, 3232, read_handler, 64, 1024, on_conn_close_handler);
-#else
-  rio_add_udp_fd(instance, 12345, read_handler, on_conn_close_handler);
-  rio_add_tcp_fd(instance, 3232, read_handler, 64, on_conn_close_handler);
-#endif
-  rio_start(instance);
+	rio_instance_t * instance = rio_create_routing_instance(init_instance, NULL);
+	rio_add_udp_fd(instance, 12345, read_handler, on_conn_close_handler);
+	rio_add_tcp_fd(instance, 3232, read_handler, 2, on_conn_close_handler);
 
-  return 0;
+	rio_start(instance);
+
+	return 0;
 }
 
